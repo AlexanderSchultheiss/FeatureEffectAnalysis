8a95f62b09f315aaed89f559d4e9b9b0c270da2c
Adam <adam@sse.uni-hildesheim.de>
2017-04-26 15:23:51 +0200

    Initial commit

---
+++ src/de/uni_hildesheim/sse/kernel_haven/feature_effects/FeatureEffectFinder.java
@@ -0,0 +1,269 @@
+package de.uni_hildesheim.sse.kernel_haven.feature_effects;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import de.uni_hildesheim.sse.kernel_haven.SetUpException;
+import de.uni_hildesheim.sse.kernel_haven.analysis.AbstractAnalysis;
+import de.uni_hildesheim.sse.kernel_haven.code_model.SourceFile;
+import de.uni_hildesheim.sse.kernel_haven.config.Configuration;
+import de.uni_hildesheim.sse.kernel_haven.util.CodeExtractorException;
+import de.uni_hildesheim.sse.kernel_haven.util.ExtractorException;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Conjunction;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Disjunction;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.False;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Formula;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Negation;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.True;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Variable;
+
+/**
+ * Calculates feature effects for all variables found in presence conditions.
+ *  
+ * @author Adam
+ */
+public class FeatureEffectFinder extends AbstractAnalysis {
+
+    private PcFinder pcFinder;
+    
+    public FeatureEffectFinder(Configuration config) {
+        super(config);
+        this.pcFinder = new PcFinder(config);
+    }
+    
+    /**
+     * Replaces each occurrence of a variable with a constant.
+     * 
+     * @param formula The formula to replace the variable in; this formula is not altered.
+     * @param variable The variable to replace.
+     * @param value Which constant the variable should be replaced with.
+     * @return A new Formula equal to the given formula, but with each occurrence of the variable replaced.
+     */
+    private Formula setToValue(Formula formula, String variable, boolean value) {
+        if (formula instanceof Variable) {
+            Variable var = (Variable) formula;
+            if (var.getName().equals(variable)) {
+                return (value ? new True() : new False());
+            }
+            return var;
+            
+        } else if (formula instanceof Negation) {
+            return new Negation(setToValue(((Negation) formula).getFormula(), variable, value));
+            
+        } else if (formula instanceof Disjunction) {
+            return new Disjunction(
+                    setToValue(((Disjunction) formula).getLeft(), variable, value),
+                    setToValue(((Disjunction) formula).getRight(), variable, value));
+            
+        } else if (formula instanceof Conjunction) {
+            return new Conjunction(
+                    setToValue(((Conjunction) formula).getLeft(), variable, value),
+                    setToValue(((Conjunction) formula).getRight(), variable, value));
+        } else {
+            return formula;
+            
+        }
+    }
+    
+    /**
+     * Simplifies boolean formulas a bit. The following simplification rules are done:
+     * <ul>
+     *      <li>NOT(NOT(a)) -> a</li>
+     *      <li>NOT(true) -> false</li>
+     *      <li>NOT(false) -> true</li>
+     *      
+     *      <li>true OR a -> true</li>
+     *      <li>a OR true -> true</li>
+     *      <li>false OR false -> false</li>
+     *      <li>a OR false -> a</li>
+     *      <li>false OR a -> a</li>
+     *      <li>a OR a -> a</li>
+     *      
+     *      <li>false AND a -> false</li>
+     *      <li>a AND false -> false</li>
+     *      <li>true AND true -> true</li>
+     *      <li>a AND true -> a</li>
+     *      <li>true AND a -> a</li>
+     *      <li>a AND a -> a</li>
+     * </ul>
+     * 
+     * @param f The formula to simplify.
+     * @return A new formula equal to the original, but simplified.
+     */
+    private Formula simplify(Formula f) {
+        if (f instanceof Negation) {
+            Formula nested = simplify(((Negation) f).getFormula());
+            
+            if (nested instanceof Negation) {
+                return ((Negation) nested).getFormula();
+                
+            } else if (nested instanceof True) {
+                return new False();
+                
+            } else if (nested instanceof False) {
+                return new True();
+            }
+            
+            return new Negation(nested);
+            
+        } else if (f instanceof Disjunction) {
+            Formula left = simplify(((Disjunction) f).getLeft());
+            Formula right = simplify(((Disjunction) f).getRight());
+            
+            if (left instanceof True || right instanceof True) {
+                return new True();
+                
+            } else if (left instanceof False && right instanceof False) {
+                return new False();
+                
+            } else if (left instanceof False) {
+                return right;
+                
+            } else if (right instanceof False) {
+                return left;
+                
+            } else if (left.equals(right)) {
+                return left;
+            }
+            
+            return new Disjunction(left, right);
+            
+        } else if (f instanceof Conjunction) {
+            Formula left = simplify(((Conjunction) f).getLeft());
+            Formula right = simplify(((Conjunction) f).getRight());
+            
+            if (left instanceof False || right instanceof False) {
+                return new False();
+             
+            } else if (left instanceof True && right instanceof True) {
+                return new True();
+                
+            } else if (left instanceof True) {
+                return right;
+                
+            } else if (right instanceof True) {
+                return left;
+                
+            } else if (left.equals(right)) {
+                return left;
+            }
+            
+            return new Conjunction(left, right);
+            
+        } else {
+            return f;
+            
+        }
+    }
+    
+    /**
+     * Creates a feature effect for the given variable and it's PCs.
+     * A feature effect is defined as:
+     * <code>Or over (for each PC in PCs ( PC[variable <- true] XOR PC[variable <- false] ))</code>.
+     * 
+     * 
+     * @param variable The variable that the feature effect should be calculated for.
+     * @param pcs All presence condition that the given variable appears in.
+     * @return A formula representing the feature effect of the variable.
+     */
+    private Formula buildFeatureEffefct(String variable, Set<Formula> pcs) {
+        
+        List<Formula> xorTrees = new ArrayList<>(pcs.size());
+        
+        for (Formula pc : pcs) {
+            Formula trueFormula = setToValue(pc, variable, true);
+            Formula falseFormula = setToValue(pc, variable, false);
+            
+            // xorTrees.add(new Xor(trueFormula, falseFormula));
+            
+            //    A xor B
+            // == (A || B) && (!A || !B)
+            
+            Formula atLeastOnePositive = new Disjunction(trueFormula, falseFormula);
+            Formula atLeastOneNegative = new Disjunction(new Negation(trueFormula), new Negation(falseFormula));
+            
+            xorTrees.add(new Conjunction(atLeastOnePositive, atLeastOneNegative));
+        }
+        
+        Formula result = xorTrees.get(0);
+        
+        for (int i = 1; i < xorTrees.size(); i++) {
+            result = new Disjunction(result, xorTrees.get(i));
+        }
+        
+        return simplify(result);
+    }
+    
+    /**
+     * Calculates the feature effects for each variable found in the presence conditions of the given
+     * source files.
+     * 
+     * @param files The source files that contain blocks with presence conditions.
+     * @return The feature effect for each found variable.
+     */
+    public Map<String, Formula> getFeatureEffects(List<SourceFile> files) {
+        Map<String, Set<Formula>> pcs = pcFinder.findPcs(files);
+        Map<String, Formula> result = new HashMap<>(pcs.size());
+        
+        for (String variable : pcs.keySet()) {
+            result.put(variable, buildFeatureEffefct(variable, pcs.get(variable)));
+        }
+        
+        return result;
+    }
+
+    /**
+     * Helper function to determine which variables are relevant.
+     * 
+     * @param variable The variable to check.
+     * @return Whether the variable is relevant or not.
+     */
+    private boolean isRelevant(String variable) {
+        return true;
+//        return !variable.startsWith("_");
+    }
+    
+    @Override
+    public void run() {
+        try {
+            cmProvider.start(config.getCodeConfiguration());
+            
+            List<SourceFile> files = new ArrayList<>(1000);
+            
+            SourceFile file;
+            while ((file = cmProvider.getNext()) != null) {
+                files.add(file);
+            }
+            
+            Map<String, Formula> result = getFeatureEffects(files);
+            
+            PrintStream out = createResultStream("feature_effects.csv");
+            for (Map.Entry<String, Formula> entry : result.entrySet()) {
+                if (isRelevant(entry.getKey())) {
+                    out.print(entry.getKey());
+                    out.print(";");
+                    out.print(entry.getValue());
+                    out.println();
+                }
+            }
+            out.close();
+            
+            out = createResultStream("unparsable_files.txt");
+            CodeExtractorException exc;
+            while ((exc = cmProvider.getNextException()) != null) {
+                out.println(exc.getCausingFile().getPath() + ": " + exc.getCause());
+            }
+            out.close();
+            
+        } catch (SetUpException e) {
+            LOGGER.logException("Error while starting cm provider", e);
+        } catch (ExtractorException e) {
+            LOGGER.logException("Error running extractor", e);
+        }
+    }
+
+}
---
+++ src/de/uni_hildesheim/sse/kernel_haven/feature_effects/PcFinder.java
@@ -0,0 +1,145 @@
+package de.uni_hildesheim.sse.kernel_haven.feature_effects;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import de.uni_hildesheim.sse.kernel_haven.SetUpException;
+import de.uni_hildesheim.sse.kernel_haven.analysis.AbstractAnalysis;
+import de.uni_hildesheim.sse.kernel_haven.code_model.Block;
+import de.uni_hildesheim.sse.kernel_haven.code_model.SourceFile;
+import de.uni_hildesheim.sse.kernel_haven.config.Configuration;
+import de.uni_hildesheim.sse.kernel_haven.util.CodeExtractorException;
+import de.uni_hildesheim.sse.kernel_haven.util.ExtractorException;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Conjunction;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Disjunction;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Formula;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Negation;
+import de.uni_hildesheim.sse.kernel_haven.util.logic.Variable;
+
+/**
+ * Creates a mapping variable -> presence conditions.
+ * 
+ * @author Adam
+ */
+public class PcFinder extends AbstractAnalysis {
+
+    public PcFinder(Configuration config) {
+        super(config);
+    }
+    
+    /**
+     * Goes through all presence conditions in all source files and creates a mapping
+     * variable -> all PCs that the variable appears in.
+     * 
+     * @param files The source files to go through.
+     * @return Every PC a variable is contained in; for each variable.
+     */
+    public Map<String, Set<Formula>> findPcs(List<SourceFile> files) {
+        Map<String, Set<Formula>> result = new HashMap<>();
+        
+        for (SourceFile file : files) {
+            for (Block b : file) {
+                findPcsInBlock(b, result);
+            }
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Finds all PCs in a block and recursively in all child blocks. Adds the PC to the set for
+     * all variables that are found in the PC.
+     * 
+     * @param block The block to find PCs in.
+     * @param result The result to add the PCs to.
+     */
+    private void findPcsInBlock(Block block, Map<String, Set<Formula>> result) {
+        
+        Set<Variable> vars = new HashSet<>();
+        findVars(block.getPresenceCondition(), vars);
+        
+        for (Variable var : vars)  {
+            result.putIfAbsent(var.getName(), new HashSet<>());
+            result.get(var.getName()).add(block.getPresenceCondition());
+        }
+        
+        for (Block child : block) {
+            findPcsInBlock(child, result);
+        }
+    }
+    
+    /**
+     * Finds all variables in the given formula. This recursively walks through the whole tree.
+     * 
+     * @param f The formula to find variables in.
+     * @param result The resulting set to add variables to.
+     */
+    private void findVars(Formula f, Set<Variable> result) {
+        
+        if (f instanceof Variable) {
+            result.add((Variable) f);
+            
+        } else if (f instanceof Negation) {
+            findVars(((Negation) f).getFormula(), result);
+            
+        } else if (f instanceof Disjunction) {
+            Disjunction dis = (Disjunction) f;
+            findVars(dis.getLeft(), result);
+            findVars(dis.getRight(), result);
+            
+        } else if (f instanceof Conjunction) {
+            Conjunction con = (Conjunction) f;
+            findVars(con.getLeft(), result);
+            findVars(con.getRight(), result);
+            
+        } else {
+            // ignore true and false
+        }
+        
+    }
+
+    @Override
+    public void run() {
+        try {
+            cmProvider.start(config.getCodeConfiguration());
+            
+            List<SourceFile> files = new ArrayList<>(1000);
+            
+            SourceFile file;
+            while ((file = cmProvider.getNext()) != null) {
+                files.add(file);
+            }
+            
+            Map<String, Set<Formula>> result = findPcs(files);
+            
+            PrintStream out = createResultStream("variable_pcs.csv");
+            for (Map.Entry<String, Set<Formula>> entry : result.entrySet()) {
+                out.print(entry.getKey());
+                for (Formula f : entry.getValue()) {
+                    out.print(";");
+                    out.print(f.toString());
+                }
+                out.println();
+            }
+            out.close();
+            
+            out = createResultStream("unparsable_files.txt");
+            CodeExtractorException exc;
+            while ((exc = cmProvider.getNextException()) != null) {
+                out.println(exc.getCausingFile().getPath() + ": " + exc.getCause());
+            }
+            out.close();
+            
+        } catch (SetUpException e) {
+            LOGGER.logException("Error while starting cm provider", e);
+        } catch (ExtractorException e) {
+            LOGGER.logException("Error running extractor", e);
+        }
+    }
+
+}
