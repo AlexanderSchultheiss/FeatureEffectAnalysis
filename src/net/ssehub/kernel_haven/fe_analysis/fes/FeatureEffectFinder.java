95493b2f25b47ce347f3b53a6b5b3f38bc0b9a94
Elscha <elscha@sse.uni-hildesheim.de>
2017-12-15 08:38:05 +0100

    Optimizations while creating XOR-Tree

--- src/net/ssehub/kernel_haven/fe_analysis/fes/FeatureEffectFinder.java
+++ src/net/ssehub/kernel_haven/fe_analysis/fes/FeatureEffectFinder.java
@@ -356,10 +356,21 @@ public class FeatureEffectFinder extends AnalysisComponent<VariableWithFeatureEf
             Formula atLeastOneNegative = innerElements.getDisjunction(variable);
             
             Formula xor;
-            if (atLeastOnePositive == null && atLeastOneNegative != null) {
+            /* 
+             * TODO SE: @Adam please check if null checks are still needed,
+             * DisjunctionQueue should not return any null values
+             */
+            if ((atLeastOnePositive == null || atLeastOnePositive == True.INSTANCE) && atLeastOneNegative != null) {
+                // TRUE AND atLeastOneNegative <-> atLeastOneNegative
                 xor = atLeastOneNegative;
-            } else if (atLeastOnePositive != null && atLeastOneNegative == null) {
+            } else if (atLeastOnePositive != null
+                && (atLeastOneNegative == null || atLeastOneNegative == True.INSTANCE)) {
+                
+                // TRUE AND atLeastOnePositive <-> atLeastOnePositive
                 xor = atLeastOnePositive;
+            } else if (False.INSTANCE == atLeastOnePositive || False.INSTANCE == atLeastOneNegative) {
+                // FALSE AND x <-> FALSE
+                xor = False.INSTANCE;
             } else {
                 xor = new Conjunction(atLeastOnePositive, atLeastOneNegative);
             }
