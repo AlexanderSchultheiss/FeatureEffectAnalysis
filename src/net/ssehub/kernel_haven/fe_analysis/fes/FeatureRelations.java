55f02eee338472a56489534b1ac22a5d216a84be
Elscha <elscha@sse.uni-hildesheim.de>
2018-07-04 09:09:37 +0200

    Bug fix: Clears list of discovered items after each run.

--- src/net/ssehub/kernel_haven/fe_analysis/fes/FeatureRelations.java
+++ src/net/ssehub/kernel_haven/fe_analysis/fes/FeatureRelations.java
@@ -93,6 +93,7 @@ public class FeatureRelations extends AnalysisComponent<FeatureDependencyRelatio
             } else {
                 addResult(new FeatureDependencyRelation(var.getVariable(), "TRUE"));
             }
+            varFinder.getVariableNames().clear();
         }
         
     }
