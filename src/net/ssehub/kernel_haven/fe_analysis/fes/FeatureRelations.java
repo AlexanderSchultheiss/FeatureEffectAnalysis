cdcf0a94382cddce8848db36b8697c81358732e1
Elscha <elscha@sse.uni-hildesheim.de>
2018-10-02 14:46:16 +0200

    Approach to visualize features in relation/arithmetic conditions

--- src/net/ssehub/kernel_haven/fe_analysis/fes/FeatureRelations.java
+++ src/net/ssehub/kernel_haven/fe_analysis/fes/FeatureRelations.java
@@ -2,6 +2,8 @@ package net.ssehub.kernel_haven.fe_analysis.fes;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.ssehub.kernel_haven.SetUpException;
 import net.ssehub.kernel_haven.analysis.AnalysisComponent;
@@ -19,6 +21,7 @@ import net.ssehub.kernel_haven.util.null_checks.NonNull;
  * @author Sascha El-Sharkawy
  */
 public class FeatureRelations extends AnalysisComponent<FeatureDependencyRelation> {
+    private static final Pattern OPERATOR_PATTERN = Pattern.compile("(=|<|>|>=|<=|!=|\\+|\\*|\\-|/|%|\\||&)");
 
     /**
      * Stores feature relationships without any constraints.
@@ -94,11 +97,17 @@ public class FeatureRelations extends AnalysisComponent<FeatureDependencyRelatio
                 for (String dependsOnVar : varFinder.getVariableNames()) {
                     // Do not track dependencies to value comparisons and keep only the assigned feature
                     // For instance: FEATURE=VALUE -> FEATURE
-                    int pos = dependsOnVar.indexOf("=");
-                    if (pos != -1) {
+                    Matcher matcher = OPERATOR_PATTERN.matcher(dependsOnVar);
+                    if (matcher.find()) {
+                        int pos = matcher.start();
                         // Keep only Feature
                         dependsOnVar = dependsOnVar.substring(0, pos);
                     }
+//                    int pos = dependsOnVar.indexOf("=");
+//                    if (pos != -1) {
+//                        // Keep only Feature
+//                        dependsOnVar = dependsOnVar.substring(0, pos);
+//                    }
                     if (dependsOnVar != null && !dependsOnVar.isEmpty()) {
                         // dependsOnVar is trimmed to Feature
                         dependentVars.add(dependsOnVar);
